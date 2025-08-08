290685a64a3c7aadce8131f2cfd274adbfb03525
Han Hollander <han_hollander@hotmail.nl>
2025-08-09 00:09:23 +0200

    basic pan and zoom

---
+++ scripts/Camera.cs
@@ -0,0 +1,48 @@
+using Godot;
+using System;
+using System.ComponentModel.DataAnnotations;
+
+public partial class Camera : Camera2D
+{
+
+	[Export] public float ZoomSpeed = 1.2f;
+	[Export] public float ZoomMin = 0.1f;
+	[Export] public float ZoomMax = 10.0f;
+
+	public bool IsPressed = false;
+
+	// Called when the node enters the scene tree for the first time.
+	public override void _Ready()
+	{
+	}
+
+	// Called every frame. 'delta' is the elapsed time since the previous frame.
+	public override void _Process(double delta)
+	{
+	}
+
+	public override void _Input(InputEvent inputEvent)
+	{
+		// Pan
+		if (inputEvent is InputEventMouseMotion motionEvent && IsPressed)
+		{
+			Position -= motionEvent.Relative / Zoom.X;
+		}
+
+		if (inputEvent is InputEventMouseButton buttonEvent)
+		{
+			if (buttonEvent.ButtonIndex == MouseButton.Left)
+			{
+				IsPressed = buttonEvent.IsPressed();
+			}
+			if (buttonEvent.ButtonIndex == MouseButton.WheelUp)
+			{
+				Zoom = Vector2.One * Math.Min(ZoomMax, Zoom.X * ZoomSpeed);
+			}
+			if (buttonEvent.ButtonIndex == MouseButton.WheelDown)
+			{
+				Zoom = Vector2.One * Math.Max(ZoomMin, Zoom.X * (1 / ZoomSpeed));
+			}
+		}
+    }
+}
