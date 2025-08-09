f948797123ee53081f15adc4ff415b1e9a74ef68
Han Hollander <han_hollander@hotmail.nl>
2025-08-09 11:42:37 +0200

    zoom threshold

--- scripts/Camera.cs
+++ scripts/Camera.cs
@@ -4,10 +4,12 @@ using System.ComponentModel.DataAnnotations;
 
 public partial class Camera : Camera2D
 {
+	[Signal] public delegate void ZoomInOverLimitEventHandler();
+	[Signal] public delegate void ZoomOutOverLimitEventHandler();
 
 	[Export] public float ZoomSpeed { get; set; } = 1.2f;
-	[Export] public float ZoomMin { get; set; } = 0.1f;
-	[Export] public float ZoomMax { get; set; } = 10.0f;
+	[Export] public float ZoomMin { get; set; } = 0.2f;
+	[Export] public float ZoomMax { get; set; } = 2.0f;
 
 	public bool IsPressed = false;
 
@@ -37,18 +39,41 @@ public partial class Camera : Camera2D
 			}
 			if (buttonEvent.ButtonIndex == MouseButton.WheelUp)
 			{
-				Vector2 mousePosition = GetLocalMousePosition();
-				Zoom = Vector2.One * Math.Min(ZoomMax, Zoom.X * ZoomSpeed);
-				Vector2 newMousePosition = GetLocalMousePosition();
-				Offset += mousePosition - newMousePosition;
+				Vector2 newZoom = GetNewZoom(ZoomSpeed);
+				if (newZoom.X < ZoomMax)
+				{
+					ZoomTo(newZoom);
+				}
+				else
+				{
+					EmitSignal(SignalName.ZoomInOverLimit);
+				}
 			}
 			if (buttonEvent.ButtonIndex == MouseButton.WheelDown)
 			{
-				Vector2 mousePosition = GetLocalMousePosition();
-				Zoom = Vector2.One * Math.Max(ZoomMin, Zoom.X * (1 / ZoomSpeed));
-				Vector2 newMousePosition = GetLocalMousePosition();
-				Offset += mousePosition - newMousePosition;
+				Vector2 newZoom = GetNewZoom(1 / ZoomSpeed);
+				if (newZoom.X > ZoomMin)
+				{
+					ZoomTo(newZoom);
+				}
+				else
+				{
+					EmitSignal(SignalName.ZoomOutOverLimit);
+				}
 			}
 		}
-    }
+	}
+
+	private Vector2 GetNewZoom(float zoomFactor) {
+		return Zoom * zoomFactor;
+	}
+
+	private void ZoomTo(Vector2 newZoom)
+	{
+	
+		Vector2 mousePosition = GetLocalMousePosition();
+		Zoom = newZoom;
+		Vector2 newMousePosition = GetLocalMousePosition();
+		Offset += mousePosition - newMousePosition;
+	}
 }
