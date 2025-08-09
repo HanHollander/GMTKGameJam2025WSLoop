88b0fbcfeb740c4cf4ef548be72ac89a46b0c877
Han Hollander <han_hollander@hotmail.nl>
2025-08-09 11:13:32 +0200

    basic zoom and pan

--- scripts/Camera.cs
+++ scripts/Camera.cs
@@ -5,9 +5,9 @@ using System.ComponentModel.DataAnnotations;
 public partial class Camera : Camera2D
 {
 
-	[Export] public float ZoomSpeed = 1.2f;
-	[Export] public float ZoomMin = 0.1f;
-	[Export] public float ZoomMax = 10.0f;
+	[Export] public float ZoomSpeed { get; set; } = 1.2f;
+	[Export] public float ZoomMin { get; set; } = 0.1f;
+	[Export] public float ZoomMax { get; set; } = 10.0f;
 
 	public bool IsPressed = false;
 
@@ -37,11 +37,17 @@ public partial class Camera : Camera2D
 			}
 			if (buttonEvent.ButtonIndex == MouseButton.WheelUp)
 			{
+				Vector2 mousePosition = GetLocalMousePosition();
 				Zoom = Vector2.One * Math.Min(ZoomMax, Zoom.X * ZoomSpeed);
+				Vector2 newMousePosition = GetLocalMousePosition();
+				Offset += mousePosition - newMousePosition;
 			}
 			if (buttonEvent.ButtonIndex == MouseButton.WheelDown)
 			{
+				Vector2 mousePosition = GetLocalMousePosition();
 				Zoom = Vector2.One * Math.Max(ZoomMin, Zoom.X * (1 / ZoomSpeed));
+				Vector2 newMousePosition = GetLocalMousePosition();
+				Offset += mousePosition - newMousePosition;
 			}
 		}
     }
