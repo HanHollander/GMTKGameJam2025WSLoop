aeff95cab13c870b073032e950c5d146d12674a8
Han Hollander <han_hollander@hotmail.nl>
2025-08-10 15:57:27 +0200

    ai

---
+++ scripts/Enemy.cs
@@ -0,0 +1,40 @@
+using Godot;
+using System;
+
+public partial class Enemy : Node
+{
+	private float _moveTimer = 0.0f;
+	[Export] public float TimeBetweenMoves = 20.0f;
+
+	// Called when the node enters the scene tree for the first time.
+	public override void _Ready()
+	{
+	}
+
+	// Called every frame. 'delta' is the elapsed time since the previous frame.
+	public override void _Process(double delta)
+	{
+		if (_moveTimer > TimeBetweenMoves)
+		{
+			foreach (Zoomable zoomable in GetNode<Main>("..").Zoomables)
+			{
+				foreach (Thumbnail thumbnail in zoomable.Thumbnails)
+				{
+					if (thumbnail.LinkedZoomable.ShipThumbnail.GetOccupier() != Thumbnail.Occupier.ENEMY
+						&& zoomable.ShipThumbnail.GetOccupier() == Thumbnail.Occupier.ENEMY)
+					{
+						zoomable.ConnectThumbnails(zoomable.ShipThumbnail, thumbnail);
+					}
+
+				}
+			}
+			_moveTimer = 0.0f;
+		}
+		else
+		{
+			_moveTimer += (float)delta;
+		}
+
+
+	}
+}
